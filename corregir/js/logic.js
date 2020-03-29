3dffa28f5c49e60ed844ca802a9deb75ce45f235
Shuffledex <shuffledex@gmail.com>
2020-03-29 15:37:14 -0300

    obfuscate

--- corregir/js/logic.js
+++ corregir/js/logic.js
@@ -1,96 +1,174 @@
+var _0x109d = [
+  "files",
+  "find",
+  "class",
+  "insertBefore",
+  "<h4>Posibles\x20copiones\x20(evaluacion\x20resuelta\x20desde\x20la\x20misma\x20computadora)</h4>",
+  "<b>CORRECTAS:\x20",
+  "list",
+  "stringify",
+  "</h4>",
+  "</b>\x20es\x20erroneo\x20y\x20no\x20se\x20puede\x20leer.</p>",
+  "</h5>",
+  "join",
+  "AES",
+  "decrypt",
+  "</b>",
+  "tdqrqmf2Et",
+  "finger",
+  "setAttribute",
+  "length",
+  "push",
+  "split",
+  "target",
+  "<p>",
+  "onload",
+  "</p>",
+  "ans",
+  "readAsText",
+  "addEventListener",
+  "toString",
+  "<br>",
+  "change",
+  "div",
+  "innerHTML",
+  "createElement",
+  "match",
+  "copiones",
+  "enc",
+  "alumno",
+  "<p>El\x20archivo\x20<b>",
+  "text/plain",
+  "<p>INCORRECTAS</p>",
+  "getElementById",
+  "Utf8",
+  "name"
+];
+(function(_0x56b7e8, _0x109dc7) {
+  var _0x34a987 = function(_0x242107) {
+    while (--_0x242107) {
+      _0x56b7e8["push"](_0x56b7e8["shift"]());
+    }
+  };
+  _0x34a987(++_0x109dc7);
+})(_0x109d, 0x1d0);
+var _0x34a9 = function(_0x56b7e8, _0x109dc7) {
+  _0x56b7e8 = _0x56b7e8 - 0x0;
+  var _0x34a987 = _0x109d[_0x56b7e8];
+  return _0x34a987;
+};
 (function() {
-  function handleFileSelect(evt) {
-    var files = evt.target.files;
-
-    var fingers = [];
-    var sameFingers = [];
-
-    for (var i = 0, f; (f = files[i]); i++) {
-      if (!f.type.match("text/plain")) {
+  function _0x4bba08(_0x37700c) {
+    var _0x308863 = _0x37700c[_0x34a9("0x29")][_0x34a9("0x14")];
+    var _0x3d1ebb = [];
+    var _0x4c2828 = [];
+    for (
+      var _0x4cf275 = 0x0, _0xd971e3;
+      (_0xd971e3 = _0x308863[_0x4cf275]);
+      _0x4cf275++
+    ) {
+      if (!_0xd971e3["type"][_0x34a9("0xa")](_0x34a9("0xf"))) {
         continue;
       }
-
-      var reader = new FileReader();
-
-      reader.onload = (function(theFile) {
-        return function(e) {
-          var div = document.createElement("div");
-          div.setAttribute("class", "alumno");
-          var decrypted = CryptoJS.AES.decrypt(e.target.result, "tdqrqmf2Et");
-          var data = decrypted.toString(CryptoJS.enc.Utf8).split("||");
-          if (data.length !== 6) {
-            div.innerHTML = [
-              "<p>El archivo <b>",
-              theFile.name,
-              "</b> es erroneo y no se puede leer.</p>"
-            ].join("");
+      var _0x5d8874 = new FileReader();
+      _0x5d8874[_0x34a9("0x2b")] = (function(_0x3450d9) {
+        return function(_0x40997a) {
+          var _0x420ae1 = document["createElement"]("div");
+          _0x420ae1[_0x34a9("0x25")](_0x34a9("0x16"), _0x34a9("0xd"));
+          var _0x2cc016 = CryptoJS[_0x34a9("0x20")][_0x34a9("0x21")](
+            _0x40997a[_0x34a9("0x29")]["result"],
+            _0x34a9("0x23")
+          );
+          var _0x2950cc = _0x2cc016[_0x34a9("0x4")](
+            CryptoJS[_0x34a9("0xc")][_0x34a9("0x12")]
+          )[_0x34a9("0x28")]("||");
+          if (_0x2950cc[_0x34a9("0x26")] !== 0x6) {
+            _0x420ae1[_0x34a9("0x8")] = [
+              _0x34a9("0xe"),
+              _0x3450d9[_0x34a9("0x13")],
+              _0x34a9("0x1d")
+            ]["join"]("");
           } else {
-            var nombreApellido = data[0];
-            var curso = data[1];
-            var colegio = data[2];
-            var fingerprint = data[3];
-            var fechaFin = data[4];
-            var respuestas = data[5];
-
-            var alumnoAux = fingers.find(f => f.finger === fingerprint);
-            if (alumnoAux) {
-              sameFingers.push({
-                alumno1: alumnoAux.alumno,
-                alumno2: nombreApellido
+            var _0x4423f2 = _0x2950cc[0x0];
+            var _0x592782 = _0x2950cc[0x1];
+            var _0x141fac = _0x2950cc[0x2];
+            var _0x190c9c = _0x2950cc[0x3];
+            var _0x48e615 = _0x2950cc[0x4];
+            var _0x459849 = _0x2950cc[0x5];
+            var _0x2cb18e = _0x3d1ebb[_0x34a9("0x15")](
+              _0x51a158 => _0x51a158[_0x34a9("0x24")] === _0x190c9c
+            );
+            if (_0x2cb18e) {
+              _0x4c2828[_0x34a9("0x27")]({
+                alumno1: _0x2cb18e[_0x34a9("0xd")],
+                alumno2: _0x4423f2
               });
             } else {
-              fingers.push({
-                alumno: nombreApellido,
-                finger: fingerprint
+              _0x3d1ebb[_0x34a9("0x27")]({
+                alumno: _0x4423f2,
+                finger: _0x190c9c
               });
             }
-
-            var correctas = 0;
-            var incorrectas = "";
-            var alumnoRespuestas = JSON.parse(respuestas);
-            for (var i = 0; i < alumnoRespuestas.length; i++) {
-              var respuesta = alumnoRespuestas[i].a;
-              var a = answers.find(a => a.id === alumnoRespuestas[i].id);
-              var correcta = a.ans;
-              if (respuesta === correcta) {
-                correctas = correctas + 1;
+            var _0x5d3643 = 0x0;
+            var _0x5a10fd = "";
+            var _0x4058a6 = JSON["parse"](_0x459849);
+            for (
+              var _0x3523a3 = 0x0;
+              _0x3523a3 < _0x4058a6[_0x34a9("0x26")];
+              _0x3523a3++
+            ) {
+              var _0x4a89b9 = _0x4058a6[_0x3523a3]["a"];
+              var _0x447c35 = answers["find"](
+                _0x4b5461 => _0x4b5461["id"] === _0x4058a6[_0x3523a3]["id"]
+              );
+              var _0xb41dc0 = _0x447c35[_0x34a9("0x1")];
+              if (_0x4a89b9 === _0xb41dc0) {
+                _0x5d3643 = _0x5d3643 + 0x1;
               } else {
-                incorrectas = incorrectas + alumnoRespuestas[i].q + "<br>";
+                _0x5a10fd =
+                  _0x5a10fd + _0x4058a6[_0x3523a3]["q"] + _0x34a9("0x5");
               }
             }
-
-            div.innerHTML = [
-              "<p>",
-              "<h4>" + nombreApellido + "</h4>",
-              "<h5>Curso: " + curso + ", " + colegio + "</h5>",
-              "</p>",
-              "<b>CORRECTAS: ",
-              correctas,
-              "</b>",
-              "<p>INCORRECTAS</p>",
-              incorrectas
-            ].join("");
+            _0x420ae1["innerHTML"] = [
+              _0x34a9("0x2a"),
+              "<h4>" + _0x4423f2 + _0x34a9("0x1c"),
+              "<h5>Curso:\x20" +
+                _0x592782 +
+                ",\x20" +
+                _0x141fac +
+                _0x34a9("0x1e"),
+              _0x34a9("0x0"),
+              _0x34a9("0x19"),
+              _0x5d3643,
+              _0x34a9("0x22"),
+              _0x34a9("0x10"),
+              _0x5a10fd
+            ][_0x34a9("0x1f")]("");
           }
-
-          document.getElementById("list").insertBefore(div, null);
-
-          if (sameFingers.length > 0) {
-            var div2 = document.createElement("div");
-            div2.setAttribute("class", "copiones");
-            div2.innerHTML = [
-              "<h4>Posibles copiones (evaluacion resuelta desde la misma computadora)</h4>",
-              JSON.stringify(sameFingers)
-            ].join("");
-            document.getElementById("copiones").insertBefore(div2, null);
+          document[_0x34a9("0x11")](_0x34a9("0x1a"))[_0x34a9("0x17")](
+            _0x420ae1,
+            null
+          );
+          if (_0x4c2828[_0x34a9("0x26")] > 0x0) {
+            var _0x579d00 = document[_0x34a9("0x9")](_0x34a9("0x7"));
+            _0x579d00[_0x34a9("0x25")](_0x34a9("0x16"), _0x34a9("0xb"));
+            _0x579d00[_0x34a9("0x8")] = [
+              _0x34a9("0x18"),
+              JSON[_0x34a9("0x1b")](_0x4c2828)
+            ][_0x34a9("0x1f")]("");
+            document["getElementById"](_0x34a9("0xb"))["insertBefore"](
+              _0x579d00,
+              null
+            );
           }
         };
-      })(f);
-
-      reader.readAsText(f);
+      })(_0xd971e3);
+      _0x5d8874[_0x34a9("0x2")](_0xd971e3);
     }
   }
-
-  document
-    .getElementById("files")
-    .addEventListener("change", handleFileSelect, false);
+  document[_0x34a9("0x11")]("files")[_0x34a9("0x3")](
+    _0x34a9("0x6"),
+    _0x4bba08,
+    ![]
+  );
 })();
