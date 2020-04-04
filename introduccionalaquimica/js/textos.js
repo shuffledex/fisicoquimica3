abb31439b6e80cbc84ec2918ada73eb43fe0e750
Shuffledex <shuffledex@gmail.com>
2020-04-04 20:00:14 -0300

    move textos.js

---
+++ introduccionalaquimica/js/textos.js
@@ -0,0 +1,17 @@
+var textos = {
+  titulo: "Introduccion a la Quimica",
+  tema: "Tema: <b>Uniones quimicas y fuerzas intermoleculares</b>",
+  botonEmpezar: "Empezar",
+  botonTerminar: "Finalizar",
+  tutorial:
+    "<h3>LEER ANTES DE EMPEZAR</h3><ul style='list-style-type: disc;'><li>Al presionar el boton EMPEZAR, podras resolver el cuestionario sin necesidad de tener Internet.</li><li>Si salis o actualizas la pagina no podras volver a resolver el cuestionario y se considerara no resuelto.</li><li>Una vez presionado el boton EMPEZAR, tendras un tiempo determinado para resolverlo. Finalizado ese tiempo el cuestionario se dara por terminado.</li><li>En el caso de preguntas multiple choice, la respuesta correcta es UNA sola.</li><li>Podes dejar preguntas sin contestar.</li><li>Podes modificar las respuestas cuantas veces quieras.</li><li>En la ultima pregunta veras un boton FINALIZAR, una vez apretado se dara por terminado el cuestionario y no podras modificar tus respuestas.</li><li>Por ultimo, debes descargar el archivo que el sistema genera con tus respuestas y mandalo por mail a: profemaia2020@gmail.com</li></ul>",
+  countdown: "Te quedan %M:%S para terminar",
+  finishClock: "Se termino el tiempo para contestar :(",
+  warning: "NOTA: Al presionar FINALIZAR no podrás modificar ninguna respuesta",
+  msgDescarga:
+    "Ahora descarga el archivo que se ha generado con tus respuestas y envialo al siguiente mail: profemaia2020@gmail.com",
+  btnDescarga: "DESCARGAR MI ARCHIVO",
+  blockUser: "No es posible seguir contestando preguntas :(",
+  descargarAgain:
+    "Ya has realizado el cuestionario.<br>Puedes descargarlo y enviarlo por mail a <b>profemaia2020@gmail.com</b>:<br>"
+};
