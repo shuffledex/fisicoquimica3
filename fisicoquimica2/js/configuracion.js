5a65f655df3feda55313fa3b7522f65483f593d9
Shuffledex <shuffledex@gmail.com>
2020-04-26 15:00:46 -0300

    fisicoquimica2

---
+++ fisicoquimica2/js/configuracion.js
@@ -0,0 +1,6 @@
+var config = {
+  timeInMinutes: 60,
+  numberOfQuestions: 17,
+  nombreArchivo: "respuestas",
+  cookie: "fisicoquimica2",
+};
---
+++ fisicoquimica2/js/preguntas.js
@@ -0,0 +1,94 @@
+var quest = [
+  {
+    id: 1,
+    question:
+      "La teoria cinetico-corpuscular permite explicar y reproducir el comportamiento y las propiedades de la materia. Explique brevemente que establece dicha teoria.",
+    textarea: true,
+  },
+  {
+    id: 2,
+    question: "Tiene forma propia y volumen definido",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 3,
+    question: "Ocupa todo el espacio disponible",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 4,
+    question: "Entre sus particulas predominan las fuerzas de atraccion",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 5,
+    question: "No tiene forma ni volumen propios",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 6,
+    question: "Tiene volumen definido pero no forma",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 7,
+    question: "Puede fluir y derramarse",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 8,
+    question: "Sus particulas estan en completo desorden",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 9,
+    question:
+      "Sus particulas solo tienen movimiento de vibracion en un lugar fijo",
+    choices: ["Liquido", "Solido", "Gaseoso"],
+  },
+  {
+    id: 10,
+    question: "Al encender una vela, su cera se derrite",
+    choices: ["Solidificacion", "Fusion", "Condensacion"],
+  },
+  {
+    id: 11,
+    question:
+      "Al hervir agua para preparar fideos, la tapa de la olla se empaña",
+    choices: ["Solidificacion", "Fusion", "Condensacion"],
+  },
+  {
+    id: 12,
+    question: "El agua se convierte en hielo al colocarla en el freezer",
+    choices: ["Solidificacion", "Fusion", "Condensacion"],
+  },
+  {
+    id: 13,
+    question:
+      "Las propiedades intensivas y extensivas de un material dependen de la cantidad de materia que lo forman",
+    choices: ["Verdadero", "Falso"],
+  },
+  {
+    id: 14,
+    question:
+      "En un dia de verano salis de la pileta y te sentas al sol. Despues de un rato te das cuenta de que estas totalmente seco. Que es lo que ocurrio con el agua que estaba sobre la superficie de tu cuerpo?",
+    choices: ["Se condenso", "Se evaporo", "Entro en ebullicion"],
+  },
+  {
+    id: 15,
+    question:
+      "Se relaciona con el numero de choques que hay entre las particulas del gas y contra el recipiente",
+    choices: ["Masa", "Volumen", "Temperatura", "Presion"],
+  },
+  {
+    id: 16,
+    question:
+      "Se relaciona con la distancia que existe entre las particulas de un gas",
+    choices: ["Masa", "Volumen", "Temperatura", "Presion"],
+  },
+  {
+    id: 17,
+    question: "Se relaciona con el numero de particulas de un gas",
+    choices: ["Masa", "Volumen", "Temperatura", "Presion"],
+  },
+];
